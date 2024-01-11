31f068456eee8bb17e9e8fe3a034cf3a006f2a2c
Hayley <37635523+greenough-h@users.noreply.github.com>
2024-01-11 11:57:21 -0800

    Update link properties

--- src/Footer.js
+++ src/Footer.js
@@ -6,10 +6,24 @@ export default function Footer() {
     <div className="Footer">
       <p>
         Built by
-        <a href="https://github.com/greenough-h"> Hayley Greenough</a>, sourced
-        on
-        <a href="https://github.com/greenough-h/weather-app-react"> Github</a>,
-        and hosted on
+        <a
+          href="https://github.com/greenough-h"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Hayley Greenough
+        </a>
+        , sourced on
+        <a
+          href="https://github.com/greenough-h/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Github
+        </a>
+        , and hosted on
         <a href="https://weather-app-react-88.netlify.app/"> Netlify</a>
       </p>
     </div>
