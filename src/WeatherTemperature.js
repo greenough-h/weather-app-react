e09df2d2fc2e0f8ad157e182db5d3fc6f3efbf27
Hayley <37635523+greenough-h@users.noreply.github.com>
2024-01-12 14:33:02 -0800

    Add celsius/fahrenheit toggle

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,18 +2,41 @@ import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
   const [unit, setUnit] = useState("celsius");
-  return (
-    <span className="temperature-block">
-      <div className="temp"> {props.celsius}</div>
-      <div className="unit">
-        <a href="#" className="text-decoration-none">
-          °C{" "}
-        </a>
-        |{" "}
-        <a href="#" className="text-decoration-none">
-          °F
-        </a>{" "}
-      </div>
-    </span>
-  );
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  if (unit === "celsius") {
+    return (
+      <span className="temperature-block">
+        <div className="temp"> {props.celsius}</div>
+        <div className="unit">
+          °C |{" "}
+          <a href="#" className="text-decoration-none" onClick={showFahrenheit}>
+            °F
+          </a>{" "}
+        </div>
+      </span>
+    );
+  } else {
+    return (
+      <span className="temperature-block">
+        <div className="temp"> {Math.round((props.celsius * 9) / 5 + 32)}</div>
+        <div className="unit">
+          {" "}
+          <a href="#" className="text-decoration-none" onClick={showCelsius}>
+            °C{" "}
+          </a>
+          | °F{" "}
+        </div>
+      </span>
+    );
+  }
 }
